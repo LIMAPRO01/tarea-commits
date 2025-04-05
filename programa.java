439ec7357b49a5e206947d00c5d6229303195049
LIMAPRO01 <gutierrezlimacesareduardo@gmail.com>
2025-04-04 22:59:18 -0600

    inicion de codigo

--- programa.java
+++ programa.java
@@ -1,1 +1,14 @@
+primer commit
+
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        var montoTotalCompra = 1000;
+        
+        System.out.println("¿Cual es el monto de compra?: ");
+        var montoCompra = Double.parseDouble(consola.nextLine());
+        
+        System.out.println("¿eres miembro de la tienda (true/false)? ");
+        var miembroTienda = Boolean.parseBoolean(consola.nextLine());
+        
+        var descuento = 0.0;
 
